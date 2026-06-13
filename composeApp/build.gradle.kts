d7d51ae666785f66693b66ba7bcde35eda4e3501
Ajay Chandran <ajaychandran443@gmail.com>
2026-06-13 11:18:36 +0530

    updated version name and code

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -76,8 +76,8 @@ android {
         applicationId = "com.cards.game.literature"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 13
-        versionName = "1.0.3"
+        versionCode = 14
+        versionName = "1.0.4"
     }
     buildFeatures {
         buildConfig = true
